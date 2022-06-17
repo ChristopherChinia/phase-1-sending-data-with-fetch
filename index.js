1b9df5e1a33b56292089733585cfae00f62adea9
christopherchinia <chinia214@gmail.com>
2022-06-17 10:56:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const {Body} = require("node-fetch");
+function submitData(name, email) {
+  const formData = {
+    name,
+    email
+  }
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify(formData)
+  })
+    .then((response) => response.json())
+
+    .then((jsonData) => {
+      document.body.textContent = jsonData.id;
+    })
+    .catch(message => document.body.textContent = message)
+}
